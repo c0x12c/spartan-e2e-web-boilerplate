04a4b60fa8b1fce98fc36b34b1407c5877d8f5a2
spartan-ducnguyen <duc.anh.nguyen@c0x12c.com>
2024-04-01 22:38:57 +0700

    Simplified CI steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 3 : undefined,
-  reporter: process.env.CI ? 'blob' : 'html',
+  reporter: 'html',
   use: {
     trace: 'on-first-retry',
   },
