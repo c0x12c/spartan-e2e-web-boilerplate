52a9aeef7ab5a0632fa096fcd28f419e49916fdd
spartan-hungphan <hungphan@MB-Pro---R3LWQR6TYJ.local>
2024-04-03 16:54:21 +0700

    add yarn commands, remove console log

--- src/features/common.step.ts
+++ src/features/common.step.ts
@@ -83,7 +83,6 @@ When(
   'I type secret data with key {string} to input with role {string}',
   async ({ secretDataProvider, basePage }, dataKey, inputName) => {
     const dataContent = getDataByKey(secretDataProvider.secretsData ?? dataKey, dataKey) ?? dataKey
-    console.log(dataContent)
     await basePage.fillByRoleTextbox(inputName, dataContent)
   },
 )
@@ -92,7 +91,6 @@ When(
   'I type secret data with key {string} to input with locator {string}',
   async ({ secretDataProvider, basePage }, dataKey, locator) => {
     const dataContent = getDataByKey(secretDataProvider.secretsData ?? dataKey, dataKey) ?? dataKey
-    console.log(dataContent)
     await basePage.fillByLocator(locator, dataContent)
   },
 )
